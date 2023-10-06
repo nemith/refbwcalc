c167b615c69d8f55c5e58d7441e6e028db659087
Brandon Bennett <brandon@brbe.me>
2023-10-06 13:43:00 -0600

    (mostly) work on python 3

--- refbwcalc/__init__.py
+++ refbwcalc/__init__.py
@@ -1,39 +1,38 @@
-import argparse
 import re
-import sys
-from collections import namedtuple, OrderedDict
+from collections import OrderedDict, namedtuple
 from functools import total_ordering
 
+DEFAULT_COMPARISON_BW = [
+    "100g",
+    "40g",
+    "10g",
+    "1g",
+    "100m",
+    "10m",
+    "6.176m",
+    "3.088m",
+    "1.544444444m",
+    "768k",
+    "384k",
+]
 
-#          Major, Minor, SR
-VERSION = (0,     1,     1)
-
-
-DEFAULT_COMPARISON_BW = ['100g', '40g', '10g', '1g', '100m', '10m', '6.176m',
-                         '3.088m', '1.544444444m', '768k', '384k']
-
-
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    return version
 
 @total_ordering
 class Bandwidth(object):
-    """ Object to represent and parse bandwith values"""
-    UNITS = OrderedDict([
-        ('bps', 1),
-        ('Kbps', 2**10),
-        ('Mbps', 2**20),
-        ('Gbps', 2**30),
-        ('Tbps', 2**40),
-        ('Pbps', 2**50)
-    ])
+    """Object to represent and parse bandwith values"""
+
+    UNITS = {
+        "bps": 1,
+        "Kbps": 2**10,
+        "Mbps": 2**20,
+        "Gbps": 2**30,
+        "Tbps": 2**40,
+        "Pbps": 2**50,
+    }
     # Use lower case for matching
-    MATCHING_UNITS = {x.lower(): y for (x,y) in UNITS.iteritems()}
+    MATCHING_UNITS = {x.lower(): y for (x, y) in UNITS.items()}
     # Also allow matching on the first letter ommiting bps
-    MATCHING_UNITS.update({x[0]: y for (x,y) in MATCHING_UNITS.iteritems()})
+    MATCHING_UNITS.update({x[0]: y for (x, y) in MATCHING_UNITS.items()})
 
     def __init__(self, value=None, default_unit=None):
         if value:
@@ -52,41 +51,50 @@ class Bandwidth(object):
                 except ValueError:
                     pass
 
-        if isinstance(value, (float, int, long)):
+        if isinstance(value, (float, int)):
             if default_unit:
                 if default_unit not in self.MATCHING_UNITS.keys():
-                    raise ValueError("Invalid unit: '%s'. Unit must be one of the " \
-                                     "following '%s" % 
-                                     (default_unit, ', '.join(MATCHING_UNITS.keys())))
+                    raise ValueError(
+                        "Invalid unit: '%s'. Unit must be one of the "
+                        "following '%s"
+                        % (default_unit, ", ".join(self.MATCHING_UNITS.keys()))
+                    )
                 return self.parse("%s%s" % (value, default_unit))
         else:
-            match = re.match(r'(?P<num>[\d.]+)(?P<unit>[ptgmkb](k(bps)?)?)', 
-                             value.strip(), flags=re.IGNORECASE)
+            match = re.match(
+                r"(?P<num>[\d.]+)(?P<unit>[ptgmkb](k(bps)?)?)",
+                value.strip(),
+                flags=re.IGNORECASE,
+            )
             if not match:
-                raise ValueError("invalid bandwidth string '%s'. Must be digit" \
-                                 "followed one of the following units: '%s'" % 
-                                     (value, ', '.join(MATCHING_UNITS.keys())))
-            value = int(float(match.group('num')) * 
-                   self.MATCHING_UNITS[match.group('unit').lower()])
+                raise ValueError(
+                    "invalid bandwidth string '%s'. Must be digit"
+                    "followed one of the following units: '%s'"
+                    % (value, ", ".join(self.MATCHING_UNITS.keys()))
+                )
+            value = int(
+                float(match.group("num"))
+                * self.MATCHING_UNITS[match.group("unit").lower()]
+            )
         self.bw = value
         return self
 
     def _update_optimal_unit(self):
         # Start of with value in bps
         bw = self._bw
-        for unit in self.UNITS.keys()[:-1]:
+        for unit in list(self.UNITS.keys())[:-1]:
             if bw < 1024.0:
                 break
             bw /= 1024.0
         else:
             # We ran out.  Wow that is fast bandwidth
-            unit = self.UNITS.keys()[-1]
+            unit = list(self.UNITS.keys())[-1]
         (self.value, self.unit) = (bw, unit)
 
     def format_pretty(self, precision=4):
-        return "{value:,.{precision}g}{unit}".format(value=self.value, 
-                                                     unit=self.unit,
-                                                     precision=precision)
+        return "{value:,.{precision}g}{unit}".format(
+            value=self.value, unit=self.unit, precision=precision
+        )
 
     @property
     def bw(self):
@@ -113,21 +121,24 @@ class Bandwidth(object):
             return self.bw < Bandwidth(other)
 
 
-Cost = namedtuple('Cost', ['refbw', 'compbw', 'cost', 'hops_24bit', 'hops_32bit', 'info'])
-def calculate_cost(refbw, compbw):
+Cost = namedtuple(
+    "Cost", ["refbw", "compbw", "cost", "hops_24bit", "hops_32bit", "info"]
+)
 
-    cost = refbw.bw/compbw.bw
+
+def calculate_cost(refbw, compbw):
+    cost = int(refbw.bw / compbw.bw)
     info = ""
 
     if cost < 1:
         cost = 1
         info = "less than min"
- 
+
     if cost > 65535:
         cost = 65535
         info = "greater than max"
- 
-    hops_24bit = 16777215/cost
-    hops_32bit = 4294967295/cost
- 
+
+    hops_24bit = int(16777215 / cost)
+    hops_32bit = int(4294967295 / cost)
+
     return Cost(refbw, compbw, cost, hops_24bit, hops_32bit, info)
--- refbwcalc/cli.py
+++ refbwcalc/cli.py
@@ -1,12 +1,14 @@
 import argparse
 import csv
-import re
-import sys
 import io
+import sys
+from importlib.metadata import version
+
 import blessings
 import colorama
-from refbwcalc import (Bandwidth, Cost, get_version, calculate_cost, 
-                       DEFAULT_COMPARISON_BW)
+
+from refbwcalc import DEFAULT_COMPARISON_BW, Bandwidth, Cost, calculate_cost
+
 
 class Color(object):
     def __init__(self, term, enabled):
@@ -16,13 +18,7 @@ class Color(object):
     def __getattr__(self, name):
         if self.enabled:
             return getattr(self._term, name)
-        return ''
-
-
-def unique(seq):
-    seen = set()
-    seen_add = seen.add
-    return [x for x in seq if x not in seen and not seen_add(x)]
+        return ""
 
 
 def generate_calc_list(refbw, comp_list):
@@ -36,18 +32,22 @@ def output_terminal(refbw, comp_list, color):
     term = blessings.Terminal()
     c = Color(term, color)
 
-    print "Calulating the OSPF cost values"\
-          "for reference bandwidth: '{c.bright_green}{}{c.normal}'\n".format(
-           refbw, c=c)
+    print(
+        "Calulating the OSPF cost values"
+        "for reference bandwidth: '{c.bright_green}{}{c.normal}'\n".format(refbw, c=c)
+    )
 
     for comp in generate_calc_list(refbw, comp_list):
         info = "({})".format(comp.info) if comp.info else ""
-        print "Cost at {c.bright_green}{r.compbw.value:>5,.4g}"\
-              "{c.green}{r.compbw.unit:<4}{c.normal}:  "\
-              "{c.cyan}{r.cost:5}{c.normal} {c.bright_yellow}{info:18}{c.normal} "\
-              "hops @ 24-bit {c.magenta}{r.hops_24bit:10}{c.normal}   "\
-              "hops @ 32-bit {c.magenta}{r.hops_32bit:10}{c.normal}".format(
-               r=comp, info=info, c=c)
+        print(
+            "Cost at {c.bright_green}{r.compbw.value:>5,.4g}"
+            "{c.green}{r.compbw.unit:<4}{c.normal}:  "
+            "{c.cyan}{r.cost:5}{c.normal} {c.bright_yellow}{info:18}{c.normal} "
+            "hops @ 24-bit {c.magenta}{r.hops_24bit:10}{c.normal}   "
+            "hops @ 32-bit {c.magenta}{r.hops_32bit:10}{c.normal}".format(
+                r=comp, info=info, c=c
+            )
+        )
 
 
 def output_csv(refbw, comp_list):
@@ -55,7 +55,7 @@ def output_csv(refbw, comp_list):
     csv_out = csv.DictWriter(output, Cost._fields)
     csv_out.writeheader()
     csv_out.writerows([comp._asdict() for comp in generate_calc_list(refbw, comp_list)])
-    print output.getvalue()
+    print(output.getvalue())
 
 
 def is_color_supported():
@@ -67,47 +67,75 @@ def is_color_supported():
 
 
 def main(argv=sys.argv[1:]):
-    # Test for color support to setup options 
+    # Test for color support to setup options
     # We may be able to just remove this.
     color_support = is_color_supported()
 
-    parser = argparse.ArgumentParser(description="""
-        Find optimal autocost reference bandwidth for OSPF by comparing 
-        the calulated value against common links found in modern networks.""")
-    parser.add_argument('ref_bw', metavar='REFBW',
-                        help="Reference bandwidth to calulate against")
+    parser = argparse.ArgumentParser(
+        description="""
+        Find optimal autocost reference bandwidth for OSPF by comparing
+        the calulated value against common links found in modern networks."""
+    )
+    parser.add_argument(
+        "ref_bw", metavar="REFBW", help="Reference bandwidth to calulate against"
+    )
     default_compairisons = [str(Bandwidth(x)) for x in DEFAULT_COMPARISON_BW]
-    parser.add_argument('-b', '--comparison-bw', metavar='BW', nargs='*', 
-                        default=DEFAULT_COMPARISON_BW, dest="bw_values",
-                        help="Bandwith values to test (default: %s)" % 
-                              ', '.join(default_compairisons))
-    parser.add_argument('-a', '--add-comparison', metavar='BW', nargs='*',
-                        default = [], dest="add_bw_values",
-                        help="Add additional bandwidth values to test.")
-    parser.add_argument('-n', '--no-color', action='store_false', 
-                        dest='color', default=color_support,
-                        help="Don't show any colors in the output.")
-    parser.add_argument('-c', '--color', action='store_true', dest='color', 
-                        default=color_support, help="Force output of colors.")
-    parser.add_argument('-f', '--format', choices=['normal', 'csv'], 
-                        default='normal',
-                        help="Output format. 'normal' outputs fancy formating "\
-                             "(optionally colored). 'csv' outputs CSV format "\
-                             "with header.")
-    parser.add_argument('--version', action='version', 
-                        version='%(prog)s ' + get_version())
+    parser.add_argument(
+        "-b",
+        "--comparison-bw",
+        metavar="BW",
+        nargs="*",
+        default=DEFAULT_COMPARISON_BW,
+        dest="bw_values",
+        help="Bandwith values to test (default: %s)" % ", ".join(default_compairisons),
+    )
+    parser.add_argument(
+        "-a",
+        "--add-comparison",
+        metavar="BW",
+        nargs="*",
+        default=[],
+        dest="add_bw_values",
+        help="Add additional bandwidth values to test.",
+    )
+    parser.add_argument(
+        "-n",
+        "--no-color",
+        action="store_false",
+        dest="color",
+        default=color_support,
+        help="Don't show any colors in the output.",
+    )
+    parser.add_argument(
+        "-c",
+        "--color",
+        action="store_true",
+        dest="color",
+        default=color_support,
+        help="Force output of colors.",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        choices=["normal", "csv"],
+        default="normal",
+        help="Output format. 'normal' outputs fancy formating "
+        "(optionally colored). 'csv' outputs CSV format "
+        "with header.",
+    )
+    parser.add_argument("--version", action="version", version=version("refbwcalc"))
 
-    args = parser.parse_args(argv)  
-    refbw = Bandwidth(args.ref_bw, default_unit='m')
+    args = parser.parse_args(argv)
+    refbw = Bandwidth(args.ref_bw, default_unit="m")
 
     comp_list = [Bandwidth(x) for x in args.bw_values + args.add_bw_values]
-    comp_list = unique(sorted(comp_list, reverse=True))
+    comp_list = sorted(comp_list, reverse=True)
 
-    if args.format == 'normal':
+    if args.format == "normal":
         output_terminal(refbw, comp_list, args.color)
-    elif args.format == 'csv':
+    elif args.format == "csv":
         output_csv(refbw, comp_list)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
